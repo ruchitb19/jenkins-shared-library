e141cd096648f991dec8caa4047f110d5a988c55
Ruchit bhosle <111356304+ruchitb19@users.noreply.github.com>
2026-04-05 00:30:31 +0530

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,9 +1,6 @@
 def call(username, image_name, tag = "${env.BUILD_NUMBER}") {
     def fullImage = "${username}/${image_name}:${tag}"
 
-    echo "Pushing Docker image: ${fullImage}"
-
-    sh """
-        docker push ${fullImage}
-    """
+    sh "docker tag ${local_image}:${tag} ${fullImage}"
+    sh "docker push ${fullImage}"
 }

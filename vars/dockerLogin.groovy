e082d39810f87e4f49995916fa9d4a7b5e962779
Ruchit bhosle <111356304+ruchitb19@users.noreply.github.com>
2026-04-04 18:32:45 +0530

    Create dockerLogin.groovy

---
+++ vars/dockerLogin.groovy
@@ -0,0 +1,11 @@
+def call(String credentialsId) {
+    withCredentials([usernamePassword(
+        credentialsId: credentialsId,
+        usernameVariable: 'DOCKER_USER',
+        passwordVariable: 'DOCKER_PASS'
+    )]) {
+        sh """
+            echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+        """
+    }
+}

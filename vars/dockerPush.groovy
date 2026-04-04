f3b2518a002a454ef6cc1da31c5d96b2204e2db2
Ruchit bhosle <111356304+ruchitb19@users.noreply.github.com>
2026-04-05 00:33:32 +0530

    Delete vars/dockerPush.groovy

--- vars/dockerPush.groovy
+++
@@ -1,6 +0,0 @@
-def call(username, image_name, tag = "${env.BUILD_NUMBER}") {
-    def fullImage = "${username}/${image_name}:${tag}"
-
-    sh "docker tag ${local_image}:${tag} ${fullImage}"
-    sh "docker push ${fullImage}"
-}

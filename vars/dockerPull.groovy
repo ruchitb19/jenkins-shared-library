ca843a88dee355e7121d87ed27c14bc0484daab9
Ruchit bhosle <111356304+ruchitb19@users.noreply.github.com>
2026-04-13 13:55:03 +0530

    Create dockerPull.groovy

---
+++ vars/dockerPull.groovy
@@ -0,0 +1,12 @@
+def call(images, tag = "latest") {
+    echo "Pulling Docker Images from Docker Hub"
+
+    // If single image is passed as string → convert to list
+    if (images instanceof String) {
+        images = [images]
+    }
+
+    for (image in images) {
+        sh "docker pull ${image}:${tag}"
+    }
+}
